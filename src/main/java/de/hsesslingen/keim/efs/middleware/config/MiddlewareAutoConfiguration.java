bf6c2121300676177dd2739bb8a3acc6130724c3
Ben Oesch <ben.oesch@hs-esslingen.de>
2020-08-19 11:44:16 +0200

    Added @Lazy to MiddlewareAutoConfiguration class to reduce overhead.

--- src/main/java/de/hsesslingen/keim/efs/middleware/config/MiddlewareAutoConfiguration.java
+++ src/main/java/de/hsesslingen/keim/efs/middleware/config/MiddlewareAutoConfiguration.java
@@ -32,36 +32,81 @@ import de.hsesslingen.keim.efs.middleware.provider.credentials.DefaultCredential
 import de.hsesslingen.keim.efs.middleware.consumer.ConsumerService;
 import de.hsesslingen.keim.efs.middleware.common.ServiceDirectoryProxy;
 import de.hsesslingen.keim.efs.middleware.provider.credentials.ICredentialsFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.annotation.Lazy;
 
 /**
  * AutoConfiguration class that creates necessary Beans for EFS-Middleware
  * conditionally
  *
  * @author k.sivarasah 27 Sep 2019
+ * @author b.oesch
  */
+@Lazy // Initialize the beans in this class only when they are needed.
 @Configuration
 @AutoConfigureAfter(RestUtilsAutoConfiguration.class)
 public class MiddlewareAutoConfiguration {
 
+    private static final Log log = LogFactory.getLog(MiddlewareAutoConfiguration.class);
+
+    /**
+     * The ServiceDirectoryProxy is used by the following components:
+     * <ul>
+     * <li>{@link ProviderRegistrator}: To register the configured mobility
+     * service.</li>
+     * <li>{@link ConsumerService}: To query the service directory for available
+     * services.</li>
+     * </ul>
+     * <p>
+     * Apart from that it can also be autowired directly by user-created.
+     * components.
+     *
+     * @return
+     */
     @Bean
     public ServiceDirectoryProxy serviceDirectoryProxy() {
+        log.info("Initializing ServiceDirectoryProxy bean...");
         return new ServiceDirectoryProxy();
     }
 
+    /**
+     * The ConsumerService is only needed if either the ConsumerApi is enabled,
+     * or if the user wants to use the ConsumerService directly by autowiring it
+     * into his own components. If a user soley acts as a provider, the
+     * ConsumerService is not needed at all.
+     *
+     * @return
+     */
     @Bean
     public ConsumerService consumerService() {
+        log.info("Initializing ConsumerService bean...");
         return new ConsumerService();
     }
 
+    /**
+     * The MobilityServiceConfigurationProperties contain information about the
+     * service that is provided by this middleware user. Therefore this Bean is
+     * not needed if the user soley acts as a consumer.
+     *
+     * @return
+     */
     @Bean
     @ConditionalOnMissingBean
     public IMobilityServiceConfigurationProperties mobilityServiceConfigurationProperties() {
+        log.info("Initializing MobilityServiceConfigurationProperties bean...");
         return new MobilityServiceConfigurationProperties();
     }
 
+    /**
+     * CredentialsFactories are only needed if the user s acts as a provider.
+     *
+     * @return
+     */
     @Bean
     @ConditionalOnMissingBean
     public ICredentialsFactory credentialsFactory() {
+        log.info("Initializing DefaultCredentialsFactory bean...");
         return new DefaultCredentialsFactory();
     }
 
