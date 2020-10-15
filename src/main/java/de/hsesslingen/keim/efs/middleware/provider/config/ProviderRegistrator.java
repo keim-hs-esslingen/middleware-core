55e7c7a906a5e679d9b50adf26b3b10fe7df6a65
Ben Oesch <ben.oesch@hs-esslingen.de>
2020-10-15 14:59:02 +0200

    Added endpoint for service info and plausability checks and updates to mobility service info.

--- src/main/java/de/hsesslingen/keim/efs/middleware/provider/config/ProviderRegistrator.java
+++ src/main/java/de/hsesslingen/keim/efs/middleware/provider/config/ProviderRegistrator.java
@@ -23,9 +23,17 @@
  */
 package de.hsesslingen.keim.efs.middleware.provider.config;
 
+import de.hsesslingen.keim.efs.middleware.provider.BookingApi;
+import de.hsesslingen.keim.efs.middleware.provider.CredentialsApi;
+import de.hsesslingen.keim.efs.middleware.provider.OptionsApi;
+import de.hsesslingen.keim.efs.middleware.provider.PlacesApi;
 import de.hsesslingen.keim.efs.mobility.service.MobilityService;
+import de.hsesslingen.keim.efs.mobility.service.MobilityService.API;
 import de.hsesslingen.keim.efs.mobility.utils.EfsRequest;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -50,22 +58,36 @@ public class ProviderRegistrator {
 
     private static final Logger logger = LoggerFactory.getLogger(ProviderRegistrator.class);
 
+    @Value("${middleware.provider.api.registration.retry-delay:5}")
+    private long retryDelay;
+    @Value("${middleware.provider.api.registration.disabled:false}")
+    private boolean registrationDisabled;
+    @Value("${middleware.service-directory-url:http://service-directory/api}")
+    public String baseUrl;
+
     @Autowired(required = false)
     private ProviderProperties properties;
 
-    @Value("${middleware.provider.api.registration.retry-delay:5}")
-    private long retryDelay;
-
-    @Value("${middleware.provider.api.registration.disabled:false}")
-    private boolean registrationDisabled;
-
-    @Value("${middleware.service-directory-url:http://service-directory/api}")
-    public String baseUrl;
+    // Requires the available rest controllers to check whether they exists or not.
+    @Autowired(required = false)
+    private PlacesApi placesApi;
+    @Autowired(required = false)
+    private OptionsApi optionsApi;
+    @Autowired(required = false)
+    private BookingApi bookingApi;
+    @Autowired(required = false)
+    private CredentialsApi credentialsApi;
 
     private ScheduledExecutorService executor;
     private TaskScheduler scheduler;
     private ScheduledFuture future;
 
+    /**
+     * Gets the scheduler used for provider registration. If none is created
+     * yet, it will create a new one.
+     *
+     * @return
+     */
     private TaskScheduler getScheduler() {
         if (scheduler == null) {
             logger.info("Creating task scheduler for provider registration...");
@@ -77,6 +99,9 @@ public class ProviderRegistrator {
         return scheduler;
     }
 
+    /**
+     * Shuts down the registration scheduler.
+     */
     private void shutdownScheduler() {
         if (scheduler != null) {
             scheduler = null;
@@ -88,6 +113,64 @@ public class ProviderRegistrator {
                 future = null;
             }
         }
+    }
+
+    /**
+     * Checks which of the rest controllers are actually ready and therefore
+     * available.
+     *
+     * @return
+     */
+    private Set<API> getAvailableApis() {
+        var list = EnumSet.noneOf(API.class);
+
+        if (placesApi != null) {
+            list.add(MobilityService.API.PLACES_API);
+        }
+
+        if (optionsApi != null) {
+            list.add(MobilityService.API.OPTIONS_API);
+        }
+
+        if (bookingApi != null) {
+            list.add(MobilityService.API.BOOKING_API);
+        }
+
+        if (credentialsApi != null) {
+            list.add(MobilityService.API.CREDENTIALS_API);
+        }
+
+        return list;
+    }
+
+    /**
+     * Checks which APIs were configured and whether their corresponding rest
+     * controller was actually created or not. Mismatches between configuration
+     * and reality are logged.
+     */
+    private void checkAndUpdateConfiguredApis() {
+        var availableApis = getAvailableApis();
+        var service = properties.getMobilityService();
+        var configApis = service.getApis();
+
+        for (var api : configApis) {
+            if (!availableApis.contains(api)) {
+                logger.warn("The API \"{}\" is configured as API in your mobility service properties, but it's controller was not instantiated. This can be due to some error or misconfiguration. The API will be removed from your available APIs.", api);
+            }
+        }
+
+        var toRemove = new ArrayList<API>();
+        for (var api : availableApis) {
+            if (!configApis.contains(api)) {
+                logger.warn("A rest controller was created for the API \"{}\" but this API is not configured as an available API in your mobility service properties. The API will not be shown as available to match your configuration.", api);
+                toRemove.add(api);
+            }
+        }
+
+        availableApis.removeAll(toRemove);
+
+        // Update properties with truely available APIs...
+        properties.getMobilityService().setApis(availableApis);
     }
 
     @EventListener(ApplicationReadyEvent.class)
@@ -103,6 +186,9 @@ public class ProviderRegistrator {
                     + "Specify your mobility properties to be able to provide services to consumers.");
             return;
         }
+
+        logger.info("Checking for mismatches between the configured APIs and the actually available ones...");
+        checkAndUpdateConfiguredApis();
 
         logger.info("Trying to register my service at the service-directory...");
 
